a51e523c0aded9687dd2f78df422311111c9a01b
michaelcegielka <michael.cegielka@gmx.de>
2022-05-29 01:48:25 +0200

    Animated Headline

---
+++ movingText.js
@@ -0,0 +1,20 @@
+const texts = ['HEY, WELCOME!', 'HEJ, WELCOME!', 'SALUT, BIENVENUE', 'HOLA, BIENVENIDA', 'Привет, добро пожаловать'];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type() {
+    if(count == texts.length){
+        count = 0;
+    }
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index);
+
+    document.querySelector('.hey').textContent = letter;
+    if(letter.length === currentText.length){
+        count++;
+        index = 0;
+    }
+    setTimeout(type, 200);
+}());
